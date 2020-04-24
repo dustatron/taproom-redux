08619b4bc58e0d3f542b87664c6c5b00daffff9d
Dusty McCord <Dustymccord@gmail.com>
2020-04-24 11:19:07 -0700

    add a working tap list component.

--- src/_components/Tap/Item.js
+++ src/_components/Tap/Item.js
@@ -1,10 +1,35 @@
 import React from "react";
+import { Card, ListGroup, Button, Col, Row } from "react-bootstrap";
 
-function TapItem() {
+function TapItem(props) {
+  function onMinusClick() {
+    props.onMinusClick(props.count);
+  }
+
   return (
-    <div className="TapItem">
-      <h1> TapItem </h1>
-    </div>
+    <Card>
+      <Card.Body>
+        <Card.Title>
+          [{props.count + 1}] {props.beer} by {props.brewery}{" "}
+        </Card.Title>
+        <Card.Text>
+          <Row>
+            <Col md={12} lg={8}>
+              <ListGroup horizontal>
+                <ListGroup.Item> ${props.price}</ListGroup.Item>
+                <ListGroup.Item>{props.aContent}% ac </ListGroup.Item>
+                <ListGroup.Item>{props.pints} Remaining</ListGroup.Item>
+              </ListGroup>
+            </Col>
+            <Col md={12} lg={4}>
+              <Button variant="info" className="btn-block" onClick={onMinusClick}>
+                -1 Pint
+              </Button>
+            </Col>
+          </Row>
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 }
 
--- src/_components/Tap/List.js
+++ src/_components/Tap/List.js
@@ -1,11 +1,22 @@
 import React from "react";
 import Item from "./Item";
 
-function TapList() {
+function TapList(props) {
   return (
     <div className="TapList">
-      <h1> TapList </h1>
-      <Item />
+      <h1 className="text-center"> Tap List </h1>
+      {props.tapList.map((tap, index) => (
+        <Item
+          count={index}
+          id={tap.id}
+          beer={tap.beer}
+          brewery={tap.brewery}
+          price={tap.price}
+          aContent={tap.aContent}
+          pints={tap.pints}
+          onMinusClick={props.onMinusPintClick}
+        />
+      ))}
     </div>
   );
 }
