1f7c7d1247cfe0ba5de81b86f9ccb7d5b8b5b0c2
Dusty McCord <Dustymccord@gmail.com>
2020-05-01 10:36:22 -0700

    add passing test for VIEW_KEY_DETAILS case of tool-view-reducer.

--- src/reducers/tool-view-reducer.js
+++ src/reducers/tool-view-reducer.js
@@ -3,6 +3,8 @@ export default (state = 0, action) => {
   switch (action.type) {
     case a.VIEW_KEY_ADD:
       return 0;
+    case a.VIEW_KEY_DETAILS:
+      return 1;
     default:
       return state;
   }
