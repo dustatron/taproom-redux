496ef62bc776c3029f6afebad06b20b3f4c79d64
Dusty McCord <Dustymccord@gmail.com>
2020-05-01 10:35:36 -0700

    add failing test for VIEW_KEY_DETAILS case of tool-view-reducer.

--- src/__tests__/reducers/tool-view-reducer.test.js
+++ src/__tests__/reducers/tool-view-reducer.test.js
@@ -9,4 +9,8 @@ describe('toolViewReducer', () => {
   test('Should update state to 0 on VIEW_KEY_ADD', () => {
     expect(toolViewReducer(1, { type: a.VIEW_KEY_ADD })).toEqual(0);
   });
+
+  test('Should update state to 1 on VIEW_KEY_DETAILS', () => {
+    expect(toolViewReducer(0, { type: a.VIEW_KEY_DETAILS })).toEqual(1);
+  });
 });
