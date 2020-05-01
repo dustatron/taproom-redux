97f15832a6f7367b3cbae173446ae90570337180
Dusty McCord <Dustymccord@gmail.com>
2020-05-01 08:55:10 -0700

    add ActionTypes with commands.

---
+++ src/actions/ActionTypes.js
@@ -0,0 +1,13 @@
+// ------------ keg-list-reducer -------------
+export const ADD_KEY = 'ADD_KEY';
+export const DELETE_KEY = 'DELETE_KEY';
+export const SELL_PINT = 'SELL_PINT';
+
+// ------------- selected-list-reducer -----------
+export const CHANGE_SELECTED = 'CHANGE_SELETED';
+
+// ------------- show-modal-reducer -----------
+export const TOGGLE_MODAL = 'TOGGLE_MODAL';
+
+// ------------- tool-view-reducer -----------
+export const CHANGE_VIEW = 'CHANGE_VIEW';
