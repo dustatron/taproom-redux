8f286cd3d42227558fa97b970ea49ab3de11af92
Dusty McCord <Dustymccord@gmail.com>
2020-05-01 10:14:59 -0700

    add failing test for toggling show-modal-reducer from false to true.

--- src/__tests__/reducers/show-modal-reducer.test.js
+++ src/__tests__/reducers/show-modal-reducer.test.js
@@ -3,6 +3,10 @@ import * as a from '../../actions/ActionTypes';
 
 describe('showModal', () => {
   it('Should return default state if action type is passed to reducer', () => {
-    expect(showModal({}, { type: null })).toEqual({});
+    expect(showModal(false, { type: null })).toEqual(false);
+  });
+
+  it('Should togggle state from false to true', () => {
+    expect(showModal(false, { type: a.TOGGLE_MODAL })).toEqual(true);
   });
 });
