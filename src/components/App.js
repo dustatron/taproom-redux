53f9df5098d09cbe1312b88f80b5e29d8a89262b
Dusty McCord <Dustymccord@gmail.com>
2020-05-01 12:03:38 -0700

    fix eslint error.

--- src/components/App.js
+++ src/components/App.js
@@ -5,120 +5,125 @@ import KegAdd from './Keg/KegAdd';
 import KegDetails from './Keg/KegDetails';
 import KegEdit from './Keg/KegEdit';
 import DeleteConfirm from './Keg/DeleteConfirm';
+//redux
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
 //bootstrap styling
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showModel: null,
-      currentKeg: {},
-      toolView: 0
-    };
-  }
-
-  hangleShowAddKey = () => {
+function App(props) {
+  const handleShowAddKey = () => {
     this.setState({ toolView: 0 });
   };
 
-  handleShowBeerDetail = (id) => {
-    const thisKeg = this.state.kegs.filter((keg) => keg.id === id);
+  const handleShowBeerDetail = (id) => {
+    const thisKeg = this.props.kegs.filter((keg) => keg.id === id);
     this.setState({ currentKeg: thisKeg[0], toolView: 1 });
   };
 
-  handleMinuPint = (index) => {
-    const allKegs = this.state.kegs;
+  const handleMinuPint = (index) => {
+    const allKegs = this.props.kegs;
     if (allKegs[index].pints > 0) {
       allKegs[index].pints -= 1;
       this.setState({ kegs: allKegs });
     }
   };
 
-  handleNewKeg = (newKeg) => {
-    const newKegList = this.state.kegs.concat(newKeg);
+  const handleNewKeg = (newKeg) => {
+    const newKegList = this.props.kegs.concat(newKeg);
     this.setState({ kegs: newKegList });
   };
 
-  handleFormEdit = (updatedKeg) => {
-    const editedKegsList = this.state.kegs
+  const handleFormEdit = (updatedKeg) => {
+    const editedKegsList = this.props.kegs
       .filter((keg) => {
-        return keg.id !== this.state.currentKeg.id;
+        return keg.id !== this.props.currentKeg.id;
       })
       .concat(updatedKeg);
     this.setState({ kegs: editedKegsList, toolView: 0 });
   };
 
-  handleDeleteKeg = () => {
-    const trimmedKegList = this.state.kegs.filter((keg) => {
-      return keg.id !== this.state.currentKeg.id;
+  const handleDeleteKeg = () => {
+    const trimmedKegList = this.props.kegs.filter((keg) => {
+      return keg.id !== this.props.currentKeg.id;
     });
 
     this.setState({ kegs: trimmedKegList, toolView: 0 });
   };
 
-  handleShowEdit = () => {
+  const handleShowEdit = () => {
     this.setState({ toolView: 2 });
   };
 
-  handleClose = () => {
+  const handleClose = () => {
     this.setState({ showModel: false });
   };
-  handleShow = () => {
+  const handleShow = () => {
     this.setState({ showModel: true });
   };
-  render() {
-    let toolView;
 
-    if (this.state.toolView === 0) {
-      toolView = <KegAdd formSubmissionHandler={this.handleNewKeg} />;
-    } else if (this.state.toolView === 1) {
-      toolView = (
-        <KegDetails
-          keg={this.state.currentKeg}
-          onAddKegClick={this.hangleShowAddKey}
-          onEditClick={this.handleShowEdit}
-          onDeleteClick={this.handleShow}
-        />
-      );
-    } else if (this.state.toolView === 2) {
-      toolView = (
-        <KegEdit
-          onformEditClick={this.handleFormEdit}
-          keg={this.state.currentKeg}
-          onAddKegClick={this.hangleShowAddKey}
-        />
-      );
-    }
+  let toolView;
 
-    return (
-      <div className="App">
-        <Header />
-        <Container>
-          <Row>
-            <Col xs={12} md={8}>
-              <TapList
-                tapList={this.state.kegs}
-                onMinusPintClick={this.handleMinuPint}
-                onDetailClick={this.handleShowBeerDetail}
-                listAccend={this.state.listOrder}
-              />
-            </Col>
-            <Col xs={12} md={4}>
-              {toolView}
-            </Col>
-          </Row>
-        </Container>
-        <DeleteConfirm
-          onDeleteClick={this.handleDeleteKeg}
-          onClose={this.handleClose}
-          show={this.state.showModel}
-          beer={this.state.currentKeg.beer}
-        />
-      </div>
+  if (props.toolView === 0) {
+    toolView = <KegAdd formSubmissionconstHandler={handleNewKeg} />;
+  } else if (props.toolView === 1) {
+    toolView = (
+      <KegDetails
+        keg={props.currentKeg}
+        onAddKegClick={handleShowAddKey}
+        onEditClick={handleShowEdit}
+        onDeleteClick={handleShow}
+      />
     );
+  } else if (props.toolView === 2) {
+    toolView = <KegEdit onformEditClick={handleFormEdit} keg={props.currentKeg} onAddKegClick={handleShowAddKey} />;
   }
+
+  return (
+    <div className="App">
+      <Header />
+      <Container>
+        <Row>
+          <Col xs={12} md={8}>
+            <TapList
+              tapList={props.kegs}
+              onMinusPintClick={handleMinuPint}
+              onDetailClick={handleShowBeerDetail}
+              listAccend={props.listOrder}
+            />
+          </Col>
+          <Col xs={12} md={4}>
+            {toolView}
+          </Col>
+        </Row>
+      </Container>
+      <DeleteConfirm
+        onDeleteClick={handleDeleteKeg}
+        onClose={handleClose}
+        show={props.showModel}
+        beer={props.currentKeg.beer}
+      />
+    </div>
+  );
 }
 
+App.propTypes = {
+  selectedKeg: PropTypes.object,
+  KegList: PropTypes.object,
+  showModal: PropTypes.bool,
+  toolView: PropTypes.number
+};
+
+const mapStateToProps = (state) => {
+  return {
+    selectedKeg: state.selectedKegReducer,
+    kegList: state.kegListReducer,
+    showModal: state.showModalReducer,
+    toolView: state.toolViewReducer
+  };
+};
+// eslint-disable-next-line
+App = connect(mapStateToProps)(App);
 export default App;
