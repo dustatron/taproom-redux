92ac4d2c176bd1fdf525d900ffbd76c12d84e251
Dusty McCord <Dustymccord@gmail.com>
2020-05-01 09:29:43 -0700

    add padding test for adding kegs to keglist reducer.

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -2,22 +2,64 @@ import kegListReducer from '../../reducers/keg-list-reducer';
 import * as a from '../../actions/ActionTypes';
 
 describe('kegListReducer', () => {
+  const kegData = {
+    beer: 'beer',
+    brewery: 'brew',
+    price: 1,
+    aContent: 2,
+    pints: 3,
+    id: 4,
+    createAt: 1587762467285
+  };
+
+  const currentState = {
+    [1]: {
+      beer: "Real DeadMan's Ale",
+      brewery: 'Pelican Brewery',
+      price: 7,
+      aContent: 7,
+      pints: 11,
+      id: '2',
+      createAt: 1587762429777
+    },
+    [2]: {
+      beer: 'Golden Girls Pale Ale',
+      brewery: 'St. Olaf Brewery',
+      price: 10,
+      aContent: 2,
+      pints: 124,
+      id: '3',
+      createAt: 1587762424204
+    }
+  };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   });
 
   test('Should successfully add new post data to kegList', () => {
+    const { beer, brewery, price, aContent, pints, id, createAt } = kegData;
     const action = {
       type: a.ADD_KEY,
-      beer: "Dirty Dan's Pale Ale",
-      brewery: 'Mnt Brewery',
-      price: 3,
-      aContent: 9,
-      pints: 4,
-      id: '1',
-      createAt: 1587762467285
+      beer,
+      brewery,
+      price,
+      aContent,
+      pints,
+      id,
+      createAt
     };
 
-    expect(kegListReducer({}, action)).toEqual(action);
+    expect(kegListReducer({}, action)).toEqual({
+      [id]: {
+        beer: 'beer',
+        brewery: 'brew',
+        price: 1,
+        aContent: 2,
+        pints: 3,
+        id: 4,
+        createAt: 1587762467285
+      }
+    });
   });
 });
--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,23 @@
+import * as a from '../actions/ActionTypes';
+
 export default (state = {}, action) => {
-  return state;
+  const { beer, brewery, price, aContent, pints, id, createAt } = action;
+  switch (action.type) {
+    case a.ADD_KEY:
+      const newState = Object.assign({}, state, {
+        [id]: {
+          beer,
+          brewery,
+          aContent,
+          price,
+          pints,
+          id,
+          createAt
+        }
+      });
+
+      return newState;
+    default:
+      return state;
+  }
 };
