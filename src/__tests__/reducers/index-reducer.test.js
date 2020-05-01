7316008d1b1b42baeda49189dea2ca7fef7686a5
Dusty McCord <Dustymccord@gmail.com>
2020-05-01 11:18:35 -0700

    add passing test for selectedKeg update matching root-reducer.

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -7,6 +7,7 @@ import showModalReducer from '../../reducers/show-modal-reducer';
 import toolViewReducer from '../../reducers/tool-view-reducer';
 //root reducer
 import rootReducer from '../../reducers/index';
+import selectedKegReducer from '../../reducers/selected-keg-reducer';
 
 let store = createStore(rootReducer);
 
@@ -18,10 +19,13 @@ describe('rootReducer', () => {
     showModal: false,
     toolView: 0
   };
+
+  // default state
   test('Should return default state if no type is recognized', () => {
     expect(rootReducer(undefined, { type: null })).toEqual(defaultState);
   });
 
+  //keg-list-reducer
   test('Check that modified state of kegList matches rootReducer', () => {
     action = {
       type: a.ADD_KEG,
@@ -36,4 +40,24 @@ describe('rootReducer', () => {
     store.dispatch(action);
     expect(store.getState().kegList).toEqual(kegListReducer(undefined, action));
   });
+
+  // selected-keg-reducer
+  test('Check that modified state of selected keg matches rootReducer', () => {
+    action = {
+      type: a.CHANGE_SELECTED,
+      beer: "Dirty Dan's Pale Ale",
+      brewery: 'Mnt Brewery',
+      price: 3,
+      aContent: 9,
+      pints: 4,
+      id: '1',
+      createAt: 1587762467285
+    };
+    store.dispatch(action);
+    expect(store.getState().selectedKeg).toEqual(selectedKegReducer({}, action));
+  });
+
+  // show-modal-reducer
+
+  // tool-view-reducer
 });
--- src/actions/ActionTypes.js
+++ src/actions/ActionTypes.js
@@ -1,13 +1,13 @@
 // ------------ keg-list-reducer -------------
-export const ADD_KEG = 'ADD_KEG';
-export const DELETE_KEG = 'DELETE_KEG';
-export const SELL_PINT = 'SELL_PINT';
+export const ADD_KEG = 'ADD_KEG'; //takes whole object
+export const DELETE_KEG = 'DELETE_KEG'; //takes id
+export const SELL_PINT = 'SELL_PINT'; //takes id
 
 // ------------- selected-list-reducer -----------
-export const CHANGE_SELECTED = 'CHANGE_SELETED';
+export const CHANGE_SELECTED = 'CHANGE_SELETED'; // takes whole object
 
 // ------------- show-modal-reducer -----------
-export const TOGGLE_MODAL = 'TOGGLE_MODAL';
+export const TOGGLE_MODAL = 'TOGGLE_MODAL'; // switches true/false
 
 // ------------- tool-view-reducer -----------
 export const VIEW_KEY_ADD = 'VIEW_KEY_ADD'; // = 0
